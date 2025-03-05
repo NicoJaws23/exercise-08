cd63e502c2c5f0fb68ad0aa7a3433a1aa8e43603
NicoJaws23 <nico.jaworski@utexas.edu>
2025-03-05 17:24:04 -0600

    2nd commit

--- ex8Code.R
+++ ex8Code.R
@@ -7,10 +7,10 @@ skim(d)
 
 #Step 2: Plots ECV as a function of social group size,
 #longevity, weaning, and reproductive lifespan
-ECV_GS <- plot(d$ECV, d$Group_size)
-ECV_Long <- plot(d$ECV, d$Longevity)
-ECV_Wean <- plot(d$ECV, d$Weaning)
-ECV_Repro <- plot(d$ECV, d$Repro_lifespan)
+ECV_GS <- plot(data = d, ECV ~ Group_size)
+ECV_Long <- plot(data = d, ECV ~ Longevity)
+ECV_Wean <- plot(data = d, ECV ~ Weaning)
+ECV_Repro <- plot(data = d, ECV ~ Repro_lifespan)
 
 #Step 3: By hand get the beta1 and beta0
 #for ECV (Y variable) as a function of Group_size (X variable)
@@ -22,5 +22,22 @@ b1 <- cor(d$Group_size, d$ECV, use = "complete.obs") * (ECV_sd/GS_sd)
 b0 <- mean(d$ECV, na.rm = TRUE) - (b1*mean(d$Group_size, na.rm = TRUE))
 
 #Step 4: Use lm() to check results
-lm(Group_size ~ ECV, data = d, na.action = na.omit) #Does not match, ask tony?
+ECVlm <- lm(ECV ~ Group_size, data = d, na.action = na.omit) #Does not match, ask tony?
+
+#Step 5, Repeat this process for each primate radiation
+#catarrhines, platyrrhines, strepsirhines
+
+regCof <- function(df, rad, x, y){
+  group <- df |>
+    filter(Taxonomic_group = rad)
+  sdX <- group |>
+    sd(x, na.rm = TRUE)
+  sdY <- group |>
+    sd(y, na.rm = TRUE)
+  b1 <- cor(group[[x]], group[[y]]) * (sdY/sdX)
+  b0 <- mean(group[[y]], na.rm = TRUE) - (b1*mean(group[[x]], na.rm = TRUE))
+  return(b1, b0)
+}
+
+
 
