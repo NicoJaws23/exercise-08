3cda6bf9b20ede164101dc21c9bdf86e05732b27
NicoJaws23 <nico.jaworski@utexas.edu>
2025-03-05 19:00:20 -0600

    why doesnt it match?!?!?!?

--- ex8Code.R
+++ ex8Code.R
@@ -20,24 +20,45 @@ GS_sd <- sd(d$Group_size, na.rm = TRUE) #X
 
 b1 <- cor(d$Group_size, d$ECV, use = "complete.obs") * (ECV_sd/GS_sd)
 b0 <- mean(d$ECV, na.rm = TRUE) - (b1*mean(d$Group_size, na.rm = TRUE))
-
+print(paste("Beta 1 =",b1, "; Beta 0 =",b0))
 #Step 4: Use lm() to check results
 ECVlm <- lm(ECV ~ Group_size, data = d, na.action = na.omit) #Does not match, ask tony?
+print(ECVlm) #are my X and Ys mixed up?
 
 #Step 5, Repeat this process for each primate radiation
 #catarrhines, platyrrhines, strepsirhines
 
+#Calc b1 and b0 on filtered dataset by radiation
 regCof <- function(df, rad, x, y){
   group <- df |>
-    filter(Taxonomic_group = rad)
-  sdX <- group |>
-    sd(x, na.rm = TRUE)
-  sdY <- group |>
-    sd(y, na.rm = TRUE)
-  b1 <- cor(group[[x]], group[[y]]) * (sdY/sdX)
+    filter(Taxonomic_group == rad)
+  sdX <- sd(group[[x]], na.rm = TRUE)
+  sdY <- sd(group[[y]], na.rm = TRUE)
+  b1 <- cor(group[[x]], group[[y]], use = "complete.obs") * (sdY/sdX)
   b0 <- mean(group[[y]], na.rm = TRUE) - (b1*mean(group[[x]], na.rm = TRUE))
-  return(b1, b0)
+  return(c(b1, b0))
 }
 
+#Use lm() on a filtered data set
+filtLM <- function(df, rad, x, y){
+  group <- df |>
+    filter(Taxonomic_group == rad)
+  ECVlm <- lm(group[[y]] ~ group[[x]], na.action = na.omit)
+  return(ECVlm)
+}
+
+#Still not getting same values, bruh what?????
+cattys <- regCof(d, "Catarrhini", "Group_size", "ECV")
+cattysLM <- filtLM(d, "Catarrhini", "Group_size", "ECV")
+
+platys <- regCof(d, "Platyrrhini", "Group_size", "ECV")
+platysLM <- filtLM(d, "Platyrrhini", "Group_size", "ECV")
+
+strepys <- regCof(d, "Strepsirhini", "Group_size", "ECV")
+strepysLM <- filtLM(d, "Strepsirhini", "Group_size", "ECV")
+
+#Step 6: From step 3, calc standard error for slope coefficient (b1)
+#95% CI, and the p value associatd with this coefficient BY HAND
+#Also get the same info from the results of the lm() function
 
 
