6076ab79953786fc34b7f8f4077cf7232d052643
NicoJaws23 <nico.jaworski@utexas.edu>
2025-03-13 14:29:04 -0500

    final

--- ex8Code.R
+++ ex8Code.R
@@ -44,7 +44,7 @@ regCof <- function(df, rad, x, y){
     filter(Taxonomic_group == rad)
   sdX <- sd(group[[x]])
   sdY <- sd(group[[y]])
-  b1 <- cor(group[[x]], group[[y]]) * (sdY/sdX)
+  b1 <- cor(group[[x]], group[[y]], use = "complete.obs") * (sdY/sdX)
   b0 <- mean(group[[y]]) - (b1*mean(group[[x]]))
   return(c(b1, b0))
 }
@@ -59,13 +59,18 @@ filtLM <- function(df, rad, x, y){
 
 cattys <- regCof(d, "Catarrhini", "Group_size", "ECV")
 cattysLM <- filtLM(d, "Catarrhini", "Group_size", "ECV")
+print(paste("Catarrhini Beta 1:", cattys[1], "and Beta 0:", cattys[2], "by hand"))
+print(cattysLM)
 
 platys <- regCof(d, "Platyrrhini", "Group_size", "ECV")
 platysLM <- filtLM(d, "Platyrrhini", "Group_size", "ECV")
+print(paste("Platyrrhini Beta 1:", platys[1], "and Beta 0:", platys[2], "by hand"))
+print(platysLM)
 
 strepys <- regCof(d, "Strepsirhini", "Group_size", "ECV")
 strepysLM <- filtLM(d, "Strepsirhini", "Group_size", "ECV")
-
+print(paste("Strepsirhini Beta 1:", strepys[1], "and Beta 0:", strepys[2]))
+print(strepysLM)
 #Step 6: From step 3, calc standard error for slope coefficient (b1)
 #95% CI, and the p value associatd with this coefficient BY HAND
 #Also get the same info from the results of the lm() function
@@ -81,15 +86,19 @@ MSE <- SSE/df_error
 
 #Step 6.2: Calc SE of b1
 SEb1 <- sqrt(MSE/SSX)
-summary(ECVlm)
-
+print(paste("Slope coefficient standard error by hand:", SEb1))
 #Step 6.3: 95% CI for b1
-CI <- b1 + qt(p=c(0.025, 0.975), ncp=0, df=150)*SEb1
-confint(ECVlm) #Rahhh
+CIb1 <- b1 + qt(p=c(0.025, 0.975), ncp=0, df=150)*SEb1
+print(paste("Upper and lower 95% confidence intervals for slope coefficient by hand:", CIb1[1], CIb1[2]))
 #Step 6.4: p-value of b1, need t_stat (t = estimate/SE) to calc
 #p-value (p = 2*pt(t, df = nrow - 2))
 tB1 <- b1/SEb1
-pB1 <- 2*pt(tB1, df = 149, lower.tail = FALSE) #Works
+pB1 <- 2*pt(tB1, df = 149, lower.tail = FALSE)
+print(paste("p-value for slope coefficient by hand:", pB1))
+#Step 6.4: Compare results to those of the lm() function
+summary(ECVlm)
+lmCI <- confint(ECVlm)
+lmCI
 
 #Step 7: Run 1000 permutations to generate a null sampling distribution
 #for the slope coefficient, need to permute Group_size since we want to break 
@@ -97,7 +106,6 @@ pB1 <- 2*pt(tB1, df = 149, lower.tail = FALSE) #Works
 
 #Permutation test
 
-#Permute
 library(mosaic)
 library(broom)
 reps <- 1000
@@ -115,22 +123,22 @@ permSE <- sd(slopes$result)
 
 #Now we estimate the p value using the theory based method
 #This method requires the standard error of our null sampling distribution (standard
-#deviation),  comparing 2 things, so we need a one-tailed t-test
+#deviation), 
 slopes_sd <- sd(slopes$result)
 slopes_mean <- mean(slopes$result)
 t <- (slopes_mean - b1)/slopes_sd
 p_upper <- 1 - pt(abs(t), df = 150) #doing upper and lower 
 p_lower <- pt(-1 * abs(t), df = 150)
 p <- p_upper + p_lower
-print(p) #Slightly larger but still significant p-value
-summary(ECVlm) #OG slope p-value is 7.26e-11
+print(paste("p-value calulcated by hand useing theory-based method:", p)) #Slightly larger but still significant p-value
+summary(ECVlm)
 
 #Step 8: Use bootstrapping to get 95% CI for estimate of slope
 #coefficient using both the quantile and theory based method
 nb <- 1000
 b <- do(nb) * {
   b_new <- d
-  bs <- slice_sample(b_new, n = n, replace = TRUE)
+  bs <- slice_sample(b_new, n = nb, replace = TRUE)
   bm <- lm(ECV ~ Group_size, data = bs)
   tidy(bm) |>
     filter(term == "Group_size") |>
@@ -139,7 +147,7 @@ b <- do(nb) * {
 
 #CI by quantile
 ci <- quantile(b$result, probs = c(0.025, 0.975))
-
+print(paste("Upper and lower 95% confidence interval using quantile method:", ci[1], ci[2]))
 #CI by theory based method
 percent_ci <- 95
 alpha <- 1 - percent_ci/100
@@ -148,6 +156,6 @@ bootSE <- sd(b$result)
 lowerCI <- bootMean + qnorm(alpha/2)*bootSE
 upperCI <- bootMean + qnorm(1 - alpha/2)*bootSE
 ciTheory <- c(lowerCI, upperCI)
-
+print(paste("Upper and lower 95% confidence interval using theory based method:", ciTheory[1], ciTheory[2]))
 #Compare to original data
 confint(ECVlm)
